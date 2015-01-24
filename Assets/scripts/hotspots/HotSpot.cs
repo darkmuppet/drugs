93fba498806abb7d6096aa39386134a8532353a8
Patrick Weiner <pawe@sbox.tugraz.at>
2015-01-24 12:50:20 +0100

    hotspot

---
+++ Assets/scripts/hotspots/HotSpot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class HotSpot : MonoBehaviour
+{
+
+    void OnMouseOver()
+    {
+        HighlightHotSpot();
+    }
+
+    void OnMouseUpAsButton()
+    {
+        OnHotSpotClicked();
+    }
+
+    void Update()
+    {
+        // TODO change highlight button
+        if (Input.GetMouseButtonDown(1))
+        {
+            HighlightHotSpot();
+        }
+    }
+
+    void HighlightHotSpot()
+    {
+        // TODO glow or something
+    }
+
+    protected abstract void OnHotSpotClicked();
+
+}
