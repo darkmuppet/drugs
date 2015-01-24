3b810140c4f9807cb4683fbe0b8daa45440fbd77
Patrick Weiner <pawe@sbox.tugraz.at>
2015-01-24 16:22:13 +0100

    DummyScripts

---
+++ Assets/scripts/hotspots/room0/HotSpotCage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotSpotCage : HotSpot {
+
+
+    public override void OnPerformAction()
+    {
+        // TODO play bird/cage animation, move cat
+    }
+}
