41db88383e019cc25ff712a71cef27061ba9b64b
Patrick Weiner <pawe@sbox.tugraz.at>
2015-01-25 12:33:46 +0100

    Bird cage fight

--- Assets/scripts/hotspots/room0/HotSpotStairs.cs
+++ Assets/scripts/hotspots/room0/HotSpotStairs.cs
@@ -5,6 +5,7 @@ public class HotSpotStairs : HotSpot {
     public override IEnumerator OnPerformAction()
     {
         Debug.Log(("FINISHED LEVEL!"));
+        Application.LoadLevel("room_2");
       yield return null;
 
     }
