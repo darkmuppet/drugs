6465a5d16d20ca12ee18170e621532c428c8e13e
Patrick Weiner <pawe@sbox.tugraz.at>
2015-01-25 11:58:02 +0100

    room updates

---
+++ Assets/scripts/hotspots/room1/HotSpotHole.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotSpotHole : HotSpot
+{
+    public GameObject doorGameObject;
+    public override IEnumerator OnPerformAction()
+    {
+
+        // TODO activate door
+        doorGameObject.SetActive(true);
+        yield return null;
+    }
+}
