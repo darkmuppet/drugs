ceec051326c524cf334a8b9bcb64d5cae692f67c
Patrick Weiner <pawe@sbox.tugraz.at>
2015-01-25 15:08:08 +0100

    cat vomiting

--- Assets/scripts/hotspots/room0/HotSpotCat.cs
+++ Assets/scripts/hotspots/room0/HotSpotCat.cs
@@ -7,12 +7,41 @@ public class HotSpotCat : HotSpot
     public GameObject vomitHotSpot;
     public GameObject vomitGameObject;
 
+    public GameObject catSideGameObject;
+    public GameObject catFrontGameObject;
+
     public override IEnumerator OnPerformAction()
     {
         
         // TODO cat vomits animation
+
+        Vector3 newCatPosFront = new Vector3(-6.02f, -5.4f, catSideGameObject.transform.position.z);
+        Vector3 newCatPos = new Vector3(-6.02f, -5.4f, catSideGameObject.transform.position.z);
+
+
+
+        // TODO triggr walk
+        Animator catAnimator = catSideGameObject.GetComponentInChildren<Animator>();
+        catAnimator.SetTrigger("catwalk");
+        iTween.MoveTo(catSideGameObject, newCatPos, 2f);
+        yield return new WaitForSeconds(2);
+        // TODO trigger idle
+        catSideGameObject.SetActive(false);
+
+        catFrontGameObject.transform.position = newCatPosFront;
+        Animator catFrontAnimator = catFrontGameObject.GetComponent<Animator>();
+
+
+
+        catFrontGameObject.SetActive(true);
+        catFrontAnimator.SetTrigger("catvomit");
+        yield return new WaitForSeconds(2);
+
+
         vomitHotSpot.SetActive(true);
         vomitGameObject.SetActive(true);
+
+        catFrontAnimator.SetTrigger("catidle");
 
         yield return null;
     }
