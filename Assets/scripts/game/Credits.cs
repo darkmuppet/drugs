b29bfa0c8f1d91487c8677c1d098f717dd0bec52
rangermann <rainer@rarebyte.com>
2015-01-25 15:00:09 +0100

    prepared credits

---
+++ Assets/scripts/game/Credits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+  public float creditsDelay = 10.0f;
+
+  public GameObject goCredits;
+
+  public void Start() {
+    goCredits.SetActive(false);
+    StartCoroutine(ShowCredits());
+  }
+
+  private IEnumerator ShowCredits() {
+    yield return new WaitForSeconds(creditsDelay);
+    goCredits.SetActive(true);
+  }
+
+}
